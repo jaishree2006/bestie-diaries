a40697e7aea415a91cb7075d86c49252d620eb26
jaishree2006 <jaishree2006happy@gmail.com>
2026-07-12 22:44:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ function startJourney(){
     document.getElementById("content").style.display = "block";
 
 }
+function showLetter(){
+
+document.getElementById("secretMessage").innerHTML =
+
+"Happy Birthday My Bestie 💗 <br><br>" +
+
+"Thank you for being such a beautiful part of my life. " +
+
+"May this new year bring you endless happiness, success, confidence and dreams coming true. ✨ <br><br>" +
+
+"Always keep smiling and shining. " +
+
+"Your best days are waiting for you 👑";
+
+}
