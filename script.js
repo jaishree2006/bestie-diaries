1866ccc102929e5f710406ef49411eeef7c0d9d9
jaishree2006 <jaishree2006happy@gmail.com>
2026-07-13 13:34:43 +0530

    Refactor music playback and update birthday message
    Updated the music playback logic and enhanced the birthday message.

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
 function startJourney(){
 
-    let music = document.getElementById("bgMusic");
-
-    music.volume = 0.5;
-
-    music.play()
-    .then(() => {
-        console.log("Music started");
-    })
-    .catch(error => {
-        console.log("Music blocked", error);
+    document.getElementById("content").scrollIntoView({
+        behavior: "smooth"
     });
 
-
-    document.querySelector(".hero").style.display="none";
-
-    document.getElementById("content").style.display="block";
-
 }
+
+
 function showLetter(){
 
-document.getElementById("secretMessage").innerHTML =
-"Happy Birthday My Bestie 💗 <br><br>" +
-"Thank you for all the beautiful memories. " +
-"May your future be filled with happiness, success and endless smiles ✨";
+    document.getElementById("secretMessage").innerHTML =
+    "Happy Birthday My Bestie 💗<br><br>" +
+    "Thank you for all the beautiful memories we created together ✨<br><br>" +
+    "May your future be filled with happiness, success and endless smiles 👑<br><br>" +
+    "Always keep shining and chasing your dreams 💕";
 
 }
